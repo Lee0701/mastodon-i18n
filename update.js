ce356d27db3a35e40e2213619b6ad3e759f4886b
Lee0701 <hgl1002@naver.com>
2021-11-09 09:14:08 +0900

    Fix update script

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs')
 
-const [baseFile, updatedFile, outFile] = process.argv.slice(2)
+const [baseFile, updatedFile] = process.argv.slice(2)
 
 const base = JSON.parse(fs.readFileSync(baseFile).toString())
 const updated = JSON.parse(fs.readFileSync(updatedFile).toString())
@@ -9,16 +9,17 @@ const updated = JSON.parse(fs.readFileSync(updatedFile).toString())
 const update = (base, updated) => {
     const entries = Object.keys(updated).map((key) => {
         if(typeof base[key] == 'object' && typeof updated[key] == 'object') {
-            return update(base[key], updated[key])
+            if(Array.isArray(base[key]) && Array.isArray(updated[key])) return [key, updated[key]]
+            else return [key, update(base[key], updated[key])]
         } else {
             if(base[key] == updated[key]) return [key, base[key]]
             else if(!base[key] && updated[key]) return [key, updated[key]]
             else if(base[key] && !updated[key]) return null
-            else return [key, updated[key]]
+            else return [key, base[key]]
         }
     }).filter((entry) => entry)
     return Object.fromEntries(entries)
 }
 
 const out = JSON.stringify(update(base, updated), null, '\t')
-fs.writeFileSync(outFile, out)
+fs.writeFileSync(baseFile, out)
