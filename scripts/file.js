98d61d3ab079a3e47e69bd6f2293d49b8f41c41e
TaYaKi71751 <shantian.tw@gmail.com>
2022-11-21 14:46:26 +0900

    Add `mkdirp` to `scripts/file.js`

--- scripts/file.js
+++ scripts/file.js
@@ -12,6 +12,26 @@ const read = (fileName) => {
 
 const write = (fileName, content) => {
     const extname = path.extname(fileName)
+    const dirname = path.dirname(fileName)
+    const mkdirp = () => {
+        if(!fs.existsSync(dirname)) return (
+            fs.mkdirSync(dirname, { recursive:true }),
+            true
+        );
+        const lstat = fs.lstatSync(dirname)
+        if(lstat.isDirectory()) return (
+            true
+        );
+        else process.abort(`'${dirname}' is not a directory. :(`)
+    }
+    switch(extname){
+        case '.json':
+        case '.yml':
+        case '.yaml':
+            mkdirp(); break;
+        default:
+            process.abort(`'${extname}' is not supported extension. :(`)
+    }
     if(extname === '.json') fs.writeFileSync(fileName, JSON.stringify(content, null, '\t'))
     else if(extname === '.yml' || extname === '.yaml') fs.writeFileSync(fileName, yaml.stringify(content))
 }
