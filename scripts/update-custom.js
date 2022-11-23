5230d2862ecf7ba1878eebb62a880239d260e331
TaYaKi71751 <shantian.tw@gmail.com>
2022-11-23 11:16:11 +0900

    Fix recursion call on `scripts/update-custom.js`

--- scripts/update-custom.js
+++ scripts/update-custom.js
@@ -13,7 +13,7 @@ const update = (current_new_orig, current_old_orig, current_old_custom) => {
     const entries = Object.keys(current_new_orig).map((key) => {
         if(typeof current_new_orig[key] == 'object' && typeof current_old_orig[key] == 'object' && typeof current_old_custom[key] == 'object') {
             if(Array.isArray(current_old_orig[key]) && Array.isArray(current_new_orig[key]) && Array.isArray(current_old_custom[key])) return [key, current_new_orig[key]]
-            else return [key, update(current_old_orig[key], current_new_orig[key], current_old_custom[key])]
+            else return [key, update(current_new_orig[key], current_old_orig[key], current_old_custom[key])]
         } else {
             if(current_new_orig[key] == current_old_orig[key]) return [key, current_old_custom[key]]
             else if(!current_old_orig[key] && current_new_orig[key]) return [key, current_new_orig[key]]
